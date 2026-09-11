e36e15fb9d8875cd9fdae0e108e8c8635784a21b
Angel Medina <Amedina4@buffs.wtamu.edu>
2026-09-11 11:53:52 -0500

    Publisher, Book, and Review

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    # Extra field of our choosing: stores a number with two decimal places
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    # Relationship: A publisher has many books
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    body = models.TextField()
+    
+    # Relationship: A book has many reviews
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Review for {self.book.title}: {self.body[:20]}..."
